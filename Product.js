48e3daca94abf6b01ca17925e7664ef5ab00bb0f
Paulo s. Farias <159643226+Paulo-S-Farias@users.noreply.github.com>
2024-08-15 11:38:15 -0300

    Update Product.js
    corrigido 
    no dia n deu tempo de corrigir ent corrigi outro dia mais fiz no prazo

--- Product.js
+++ Product.js
@@ -1,32 +1,31 @@
 class Product {
-    constructor(name, description, price) {
-        this.name = name
-        this.description = description
-        this.price = price
-        this.inStock = 0
-    }
+  constructor(name, description, price) {
+      this.name = name
+      this.description = description
+      this.price = price
+      this.inStock = 0
+  }
 
-    addToStock(quantity) {
-        this.inStock += quantity
-    }
+  addToStock(quantity) {
+      this.inStock += quantity
+      console.log(`
+          Nome do produto: ${this.name}
+          Descrição do produto: ${this.description}
+          Valor do produto: R$${this.price.toFixed(2)}
+          Quantidade do produto: ${this.inStock}
+      `)
+  }
 
-    calculateDiscount(discountPercentage) {
-        const discountAmount = (this.price * discountPercentage) / 100
-        return this.price - discountAmount
-    }
+  calculateDiscount(discount) {
+      this.price = this.price - (discount * this.price / 100)
+      console.log(`
+          ===========================
+          Desconto no produto: ${discount}%
+          Novo valor do produto: ${this.price.toFixed(2)}
+      `)
+  }
 }
 
-const product = new Product('Notebook', 'Notebook Nitro 7', '2000')
-product.addToStock (10)
-
-console.log(`Nome do produto: ${product.name}`)
-console.log(`Descrição do produto: ${product.description}`)
-console.log(`Valor do produto: R$${product.price}.00`)
-console.log(`Quantidade no estoque: ${product.inStock}`)
-console.log('=====================================')
-
-const discount = 10
-const discountedPrice = product.calculateDiscount(discount)
-
-console.log(`Desconto no produto: ${discount}%`)
-console.log(`Novo valor do produto: R$${discountedPrice}.00`)
+const notebook = new Product('Notebook', 'Notebook Nitro 5', 1000)
+notebook.addToStock(10)
+notebook.calculateDiscount(5)
